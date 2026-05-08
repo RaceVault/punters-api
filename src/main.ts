72a8cc2764d01f28ae9fad7e4cbcf64bff392382
nafsonig <saurinkudelivery@gmail.com>
2026-05-08 13:07:47 +0000

    implemented the rate limiting

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.set('trust proxy', true);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
